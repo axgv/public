42a977333cf3ab5ee3476b808ca092eb72c28b55
axgv <60951269+axgv@users.noreply.github.com>
2020-12-05 17:36:43 -0500

    Create background-noimages.js

---
+++ background-noimages.js
@@ -0,0 +1,5 @@
+chrome.webRequest.onBeforeRequest.addListener(function(d){
+  return {cancel:true};
+},{urls:["https://assets.*.com/*/ma/*.jpg",
+         "https://assets.*.com/*/sw/*.jpg"]},
+  ["blocking"]);
