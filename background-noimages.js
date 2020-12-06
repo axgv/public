68fdc55f554ee264bac47d2f596499206bb81d1d
axgv <60951269+axgv@users.noreply.github.com>
2020-12-05 20:32:36 -0500

    Update background-noimages.js

--- background-noimages.js
+++ background-noimages.js
@@ -1,5 +1,7 @@
+/*
 chrome.webRequest.onBeforeRequest.addListener(function(d){
   return {cancel:true};
 },{urls:["https://assets.*.com/*/ma/*.jpg",
          "https://assets.*.com/*/sw/*.jpg"]},
   ["blocking"]);
+*/
