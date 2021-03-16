a5db9b41f337a5dee6f51e201cab735638aeddd3
axgv <60951269+axgv@users.noreply.github.com>
2021-03-16 10:24:02 -0400

    Update and rename xmod1.js to xmod2.js

--- mods/xmod1.js
+++
@@ -1,1 +0,0 @@
-if $('[class="cart-button"]') { $('[class="cart-button"]').click() }; if $('[class="button checkout"]') { $('[class="button checkout"]').click() };
---
+++ mods/xmod2.js
@@ -0,0 +1,1 @@
+if ($('[class="cart-button"]')) { $('[class="cart-button"]').click() }; if ($('[class="button checkout"]')) { $('[class="button checkout"]').click() };
